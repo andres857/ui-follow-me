c5398f5f1890badddb142ffa90b388f8a201dfe3
Andres Guerrero <andres-guerrero@Andress-MacBook-Air.local>
2023-12-12 23:21:18 -0500

    card con icono de buscar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'follow': '#1C3154',
+				'Dark-gray-blue-alpha': 'hsl(224, 30%, 27%, 0.5)',
+			}
+		},
 	},
 	plugins: [],
 }
